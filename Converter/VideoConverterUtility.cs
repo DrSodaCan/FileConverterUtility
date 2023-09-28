4f0bd428d71fa361d7bcbc73c1412e5e93a9bfc1
Atlas <thegalaxy2015@yahoo.com>
2023-09-27 21:15:35 -0700

    Added a Convert To MP4 with a certain resolution option to the Video Converter Section.

--- Converter/VideoConverterUtility.cs
+++ Converter/VideoConverterUtility.cs
@@ -16,8 +16,10 @@ public class VideoConverterUtility : FileConverterUtility
         Dictionary<string, Action> conversions = new Dictionary<string, Action>()
         {
             {"mp4", () => utility.ConvertToMp4(utility._inputFile, utility._outputFolder)},
-            {"mp4 codec", () => utility.ConvertToMp4(utility._inputFile, utility._outputFolder, codec)},
+            {"mp4 codec", () => utility.ConvertToMp4Codec(utility._inputFile, utility._outputFolder, codec)},
+            {"mp4 resolution", (() => utility.ConvertToMp4Resolution(utility._inputFile, utility._outputFolder))},
             {"mov", () => utility.ConvertToMov(utility._inputFile, utility._outputFolder)},
+            
         };
 
         utility.getLocations();
@@ -32,12 +34,59 @@ public class VideoConverterUtility : FileConverterUtility
         FFMpegArguments.FromFileInput(inputFilePath).OutputToFile(mp4FilePath).ProcessSynchronously();
     }
     
-    private void ConvertToMp4(string inputFilePath, string outputFolderPath, Codec codec)
+    private void ConvertToMp4Codec(string inputFilePath, string outputFolderPath, Codec codec)
     {
         EnsureOutputDirectoryExists(outputFolderPath);
         string mp4FilePath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputFilePath) + ".mp4");
         FFMpegArguments.FromFileInput(inputFilePath).OutputToFile(mp4FilePath, true, options => options.WithVideoCodec(codec)).ProcessSynchronously();
     }
+
+    private void ConvertToMp4Resolution(string inputFilePath, string outputFolderPath)
+    {
+        int[] resolution = ResolutionStringToArr(GetResolution());
+        string mp4FilePath = Path.Combine(outputFolderPath,
+            Path.GetFileNameWithoutExtension(inputFilePath) + "_converted.mp4");
+        FFMpegArguments.FromFileInput(inputFilePath).OutputToFile(mp4FilePath, true, options => options.Resize(resolution[0], resolution[1]));
+    }
+
+    private string? GetResolution()
+    {
+        string? input;
+        Dictionary<string, string?> resolutionToHW = new Dictionary<string, string?>()
+        {
+            { "480p", "854x480" },
+            { "720p ", "1280x720" },
+            {"1080p", "1920x1080"},
+            {"1440p", "2560x1440"},
+            {"2160p", "3840x2160"},
+            {"4k", "3840x2160"}
+        };
+        Console.WriteLine("Enter a resolution. Allowed formats: \"xxxp\" or \"widthxheight\"");
+        input = Console.ReadLine().ToLower();
+        //Exit case
+        if (input == "" || input == "exit")
+        {
+            return null;
+        }
+        if (resolutionToHW.ContainsKey(input))
+        {
+            return resolutionToHW[input];
+        }else if (resolutionToHW.ContainsValue(input))
+            return input;
+
+        Console.WriteLine("Resolution not found!");
+            return GetResolution();
+    }
+
+    private int[] ResolutionStringToArr(string input)
+    {
+        int[] output = {0, 0};
+        string[] tempStrings = input.Split(" ");
+        output[0] = Int32.Parse(tempStrings[0]);
+        output[1] = Int32.Parse(tempStrings[1]);
+        return output;
+    }
+    
 
     private void ConvertToMov(string inputFilePath, string outputFolderPath)
     {
